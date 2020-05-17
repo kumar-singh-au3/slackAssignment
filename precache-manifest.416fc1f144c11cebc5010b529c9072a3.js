2b57c5e95939a4fd22133097a96c6ed866a7b69f
kumar-singh-au3 <manassingh26@gmail.com>
2020-05-17 18:55:47 +0530

    Updates

---
+++ precache-manifest.416fc1f144c11cebc5010b529c9072a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f38aa5ad1a7abdcd3a1098f5038c56d",
+    "url": "/wwse/index.html"
+  },
+  {
+    "revision": "8b67bfa4ef911c204e43",
+    "url": "/wwse/static/css/main.9088c277.chunk.css"
+  },
+  {
+    "revision": "8f415a610405624fdc63",
+    "url": "/wwse/static/js/2.058154a0.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/wwse/static/js/2.058154a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b67bfa4ef911c204e43",
+    "url": "/wwse/static/js/main.2285a0ea.chunk.js"
+  },
+  {
+    "revision": "2ede3061605ffd90b7fb",
+    "url": "/wwse/static/js/runtime-main.3807d83d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wwse/static/media/logo.5d5d9eef.svg"
+  }
+]);
